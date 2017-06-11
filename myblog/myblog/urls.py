bf049cdd80e6019f0998e33948ef63dde3149e14
YUNFEI YANG <yunfei.yang717@gmail.com>
2017-06-11 14:51:42 -0500

    finish about page and it could connected to home page and could refresh

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from posts import views
+import posts.views
+import sitepages.views
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home),
-    url(r'^posts/(?P<post_id>[0-9]+)/$', views.post_details, name="post_detail"),
+    url(r'^$', posts.views.home, name="home"),
+    url(r'^posts/(?P<post_id>[0-9]+)/$', posts.views.post_details, name="post_detail"),
+    url(r'^about/', sitepages.views.about, name="about"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
